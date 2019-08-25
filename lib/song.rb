1342db5eff3210c7fade5b9dff3bcd36c452f965
Daniel  Bovington <dbovington@daniels-imac.home>
2019-08-25 12:06:14 +0100

    "trying to submit"

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,18 @@ require_relative "./interactive_record.rb"
 
 class Song < InteractiveRecord
 
+  attr_accessor :id, :name, :album
+
+  def initialize(options={})
+  options.each do |property, value|
+    self.send("#{property}=", value)
+    end
+  end
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
